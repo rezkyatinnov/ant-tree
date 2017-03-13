7de450d7668109b7e8aac5cd2d0b6488f22a3cb4
Mohamad Rezkya Feurstinnov Hairy <rezkyatinnov@gmail.com>
2017-03-14 00:56:32 +0800

    add action mock, add monitor layout

--- routes/auth.js
+++ routes/auth.js
@@ -3,10 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/booth', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+	res.render('login', { title: 'Booth Login', loginAction:'booth' });
 });
 router.get('/admin', function(req, res, next) {
-  res.render('login', { title: 'Express' });
+	res.render('login', { title: 'Admin Login', loginAction:'admin' });
+});
+router.post('/booth', function(req, res, next) {
+	res.redirect("/transaction");
+});
+router.post('/admin', function(req, res, next) {
+	res.redirect("/admin/dashboard");
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'AntTree' });
 });
+router.get('/monitor', function(req, res, next) {
+  res.render('monitor', { title: 'AntTree Monitor' });
+});
+
+
 
 module.exports = router;
